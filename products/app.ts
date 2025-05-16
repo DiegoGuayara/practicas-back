6584da7c33e4ccd3f6cf40939aad82d2ba8d9bca
DiegoGuayara <diegofguayara9707@gmail.com>
2025-05-16 16:19:57 -0500

    Hola

--- products/app.ts
+++ products/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import productsRouter from "./routes/product.routes";
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 10102;
 
 app.use(express.json());
+app.use(cors());
 app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
